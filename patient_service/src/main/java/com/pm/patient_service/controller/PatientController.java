88266a3473fad80164a4fbb8b6a46266e903b195
mohammed kebdani <kebdaniserjhnx@gmail.com>
2026-01-31 06:26:08 +0100

    fixing the validation for create and update

--- patient_service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ patient_service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.pm.patient_service.controller;
 
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
 import com.pm.patient_service.service.PatientService;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
@@ -30,8 +31,11 @@ public class PatientController {
     }
 
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+    public ResponseEntity<PatientResponseDTO> createPatient(
+            @Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+
         return ResponseEntity.ok().body(patientResponseDTO);
     }
 
